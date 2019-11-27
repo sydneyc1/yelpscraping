82e7128f4c64167bd279506f6de23e32189680e1
sydneyc1 <57411277+sydneyc1@users.noreply.github.com>
2019-11-26 21:10:53 -0600

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -18,56 +18,43 @@ price = []
 
 for main in mains:
     try:
-        Bizname.append(main.address.find("a", {"class": "lemon--a__373c0__IEZFH link__373c0__29943 "
-                                                       "link-color--blue-dark__373c0__1mhJo "
-                                                       "link-size--inherit__373c0__2JXk5"}).text)
+        Bizname.append(main.find("a", {"class": "lemon--a__374c0__IEZFH link__373c0__29943 "
+                                                "link-color--blue-dark__373c0__1mhJo "
+                                                "link-size--inherit__373c0__2JXk5"}).text)
     except:
         Bizname.append("None")
     try:
-        ratings.append(main.find("div", {"class": "lemon--div__373c0__1mboc i-stars__373c0__Y2F3O "
-                                                  "i-stars--regular-4-half__373c0__3VLp8 "
-                                                  "border-color--default__373c0__2oFDT "
-                                                  "overflow--hidden__373c0__8Jq2I"}).div.get('aria-label'))
+        ratings.append(main.find("div", {"class": "lemon--div__373c0__1mboc"}).div.get('aria-label'))
     except:
         ratings.append("None")
 
     try:
-        price.append(main.find("span", {"class": "lemon--span__373c0__3997G text__373c0__2pB8f "
-                                                 "priceRange__373c0__2DY87 text-color--normal__373c0__K_MKN "
-                                                 "text-align--left__373c0__2pnx_ "
-                                                 "text-bullet--after__373c0__1ZHaA"}).text)
+        price.append(main.find("span", {
+            "class": "lemon--span__373c0__3997G text__373c0__2pB8f priceRange__373c0__2DY87 text-color--normal__373c0__K_MKN text-align--left__373c0__2pnx_ text-bullet--after__373c0__1ZHaA"}).text)
     except:
         price.append("None")
 
 secondarys = page_soup.find_all("div", {"class": "lemon--div__373c0__1mboc secondaryAttributes__373c0__7bA0w "
                                                  "arrange-unit__373c0__1piwO border-color--default__373c0__2oFDT"})
 sec = secondarys[0]
-
-# Empty list for secondary list items
-
 add = []
-tel = []
-
-# Get Secondary attributes (add, tel)
 
 for sec in secondarys:
     try:
         add.append(sec.address.find("span", {"class": "lemon--span__373c0__3997G"}).text)
     except:
         add.append("None")
-    try:
-        tel.append(sec.div.div.text)
-    except:
-        tel.append("None")
 
-# Replace any non-telephone numbers with "None"
+print(Bizname)
+print(ratings)
+print(price)
+print(add)
 
-# new_tel = [x if (bool(re.search(r'[(]\d\d[)].\d{4}.\d{4}|[(]\d\d[)].\d{4}.\d{3}|\d{4}.\d{3}.\d{3}', x)) == True) else "None" for x in tel]
-
-
-data = {}
-a = {'Rest_name': Bizname, 'Rest_ratings': ratings, 'Rest_price': price, 'Rest_add': add}
-rest = pd.DataFrame.from_dict(a, orient='index')
-rest.transpose()
-rest = pd.DataFrame(data)
-rest.to_csv('Output.csv', columns=('Rest_name', 'Rest_ratings', 'Rest_price', 'Rest_add'))
+# data = {}
+# a = {'Rest_name': Bizname, 'Rest_ratings': ratings, 'Rest_price': price, 'Rest_add': add}
+# orient = 'index'
+# rest = pd.DataFrame.from_dict(a, orient)
+# rest.transpose()
+# rest = pd.DataFrame(data)
+# columns = ('Rest_name', 'Rest_ratings', 'Rest_price', 'Rest_add')
+# rest.to_csv('Output.csv', columns)
